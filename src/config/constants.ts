87a8559b42ebd695bdc5e7c2fd8cc41ff593a1e2
efrain2010 <efrain.villanueva3@gmail.com>
2020-12-08 17:06:34 +0000

    Adding product categories to the menu and constants

---
+++ src/config/constants.ts
@@ -0,0 +1,22 @@
+export const productCategories = [
+  {
+    id: 'cat-1',
+    slug: 'men-clothing',
+    name: 'men clothing',
+  },
+  {
+    id: 'cat-2',
+    slug: 'jewelery',
+    name: 'jewelery',
+  },
+  {
+    id: 'cat-3',
+    slug: 'electronics',
+    name: 'electronics',
+  },
+  {
+    id: 'cat-4',
+    slug: 'women-clothing',
+    name: 'women clothing',
+  },
+]
