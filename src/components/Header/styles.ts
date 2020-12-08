ffa50f248b482dc8925b587c9764ed5a9323c06a
efrain2010 <efrain.villanueva3@gmail.com>
2020-12-08 12:11:08 +0000

    Added theme type for typescript

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,17 +1,19 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    paddingBottom: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    paddingTop: theme.spacing(1),
-    borderBottom: '1px solid #ccc',
-  },
-  logo: {
-    width: 250,
-  },
-  search: {},
-}))
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      paddingBottom: theme.spacing(1),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+      paddingTop: theme.spacing(1),
+      borderBottom: '1px solid #ccc',
+    },
+    logo: {
+      width: 250,
+    },
+    search: {},
+  })
+)
 
 export default useStyles
