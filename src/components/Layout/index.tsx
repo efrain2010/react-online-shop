4eebb45da4d246691ffe32ce0b313cc4dd898df4
efrain2010 <efrain.villanueva3@gmail.com>
2020-12-08 12:13:26 +0000

    Added routes component for routing system

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import Routes from '../Routes'
 import Header from '../Header'
 
 const Layout = () => {
   return (
     <>
+      <CssBaseline />
       <Header />
+      <main>
+        <Routes></Routes>
+      </main>
     </>
   )
 }
