f06d0d828f99c4e28f561196c2a5f5b205830c8d
efrain2010 <efrain.villanueva3@gmail.com>
2020-12-08 12:10:35 +0000

    Added react router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Layout from '../Layout'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import useStyles from '../../config/theme'
 
 const App = () => {
   useStyles()
 
   return (
-    <Router>
+    <BrowserRouter>
       <Layout />
-    </Router>
+    </BrowserRouter>
   )
 }
 
